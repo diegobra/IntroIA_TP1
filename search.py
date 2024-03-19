ab66d4015793313c9c96b827f50e531db559c523
Diego Braga <diegobra20@gmail.com>
2024-03-19 19:56:44 -0300

    Commit inicial

---
+++ search.py
@@ -0,0 +1,100 @@
+from collections import deque
+import tree_hanoi
+import hanoi_states
+
+
+def breadth_first_tree_search(problem: hanoi_states.ProblemHanoi):
+    """
+    Realiza una búsqueda en anchura para encontrar una solución a un problema de Hanoi.
+    Esta función no chequea si un estado se visito, por lo que puede entrar en Loop infinitos muy fácilmente. No
+    usarla con más de 3 discos.
+
+    Parameters:
+        problem (hanoi_states.ProblemHanoi): El problema de la Torre de Hanoi a resolver.
+
+    Returns:
+        tree_hanoi.NodeHanoi: El nodo que contiene la solución encontrada.
+    """
+    frontier = deque([tree_hanoi.NodeHanoi(problem.initial)])  # Creamos una cola FIFO con el nodo inicial
+    while frontier:
+        node = frontier.popleft()  # Extraemos el primer nodo de la cola
+        if problem.goal_test(node.state):  # Comprobamos si hemos alcanzado el estado objetivo
+            return node
+        frontier.extend(node.expand(problem))  # Agregamos a la cola todos los nodos sucesores del nodo actual
+
+    return None
+
+
+def breadth_first_graph_search(problem: hanoi_states.ProblemHanoi, display: bool = False):
+    """
+    Realiza una búsqueda en anchura para encontrar una solución a un problema de Hanoi. Pero ahora si recuerda si ya
+    paso por un estado e ignora seguir buscando en ese nodo para evitar recursividad.
+
+    Parameters:
+        problem (hanoi_states.ProblemHanoi): El problema de la Torre de Hanoi a resolver.
+        display (bool, optional): Muestra un mensaje de cuantos caminos se expandieron y cuantos quedaron sin expandir.
+                                  Por defecto es False.
+
+    Returns:
+        tree_hanoi.NodeHanoi: El nodo que contiene la solución encontrada.
+    """
+
+    frontier = deque([tree_hanoi.NodeHanoi(problem.initial)])  # Creamos una cola FIFO con el nodo inicial
+
+    explored = set()  # Este set nos permite ver si ya exploramos un estado para evitar repetir indefinidamente
+    while frontier:
+        node = frontier.popleft()  # Extraemos el primer nodo de la cola
+
+        # Agregamos nodo al set. Esto evita guardar duplicados, porque set nunca tiene elementos repetidos, esto sirve
+        # porque heredamos el método __eq__ en tree_hanoi.NodeHanoi de aima.Node
+        explored.add(node.state)
+
+        if problem.goal_test(node.state):  # Comprobamos si hemos alcanzado el estado objetivo
+            if display:
+                print(len(explored), "caminos se expandieron y", len(frontier), "caminos quedaron en la frontera")
+            return node
+        # Agregamos a la cola todos los nodos sucesores del nodo actual que no haya visitados
+        frontier.extend(child for child in node.expand(problem)
+                        if child.state not in explored and child not in frontier)
+
+    return None
+
+def depth_first_graph_search(problem: hanoi_states.ProblemHanoi, display: bool = False):
+    """
+    Resolución a la pregunta 7 del TP1.
+    Se implemente una búsqueda en profundidad tomando como base el algoritmo ya implementado
+    en la función breadth_first_graph_search.
+
+    En este caso se utiliza una cola LIFO para almacenar los nodos en la frontera, lo que hace que siempre
+    se recorran primero los hijos en lugar de dejarlos para el final.
+
+    Parameters:
+        problem (hanoi_states.ProblemHanoi): El problema de la Torre de Hanoi a resolver.
+        display (bool, optional): Muestra un mensaje de cuantos caminos se expandieron y cuantos quedaron sin expandir.
+                                  Por defecto es False.
+
+    Returns:
+        tree_hanoi.NodeHanoi: El nodo que contiene la solución encontrada.
+    """
+
+    frontier = deque([tree_hanoi.NodeHanoi(problem.initial)])  # Se crea cola LIFO con el estado inciial
+
+    explored = set()  # Este set nos permite ver si ya exploramos un estado para evitar repetir indefinidamente
+    while frontier:
+        node = frontier.popleft()  # Extraemos el primer nodo de la cola
+
+        # Agregamos nodo al set. Esto evita guardar duplicados, porque set nunca tiene elementos repetidos, esto sirve
+        # porque heredamos el método __eq__ en tree_hanoi.NodeHanoi de aima.Node
+        explored.add(node.state)
+
+        if problem.goal_test(node.state):  # Comprobamos si hemos alcanzado el estado objetivo
+            if display:
+                print(len(explored), "caminos se expandieron y", len(frontier), "caminos quedaron en la frontera")
+            return node
+        # Agregamos a la cola todos los nodos sucesores del nodo actual que no haya visitados
+        # A diferencia de la búsqueda en anchura, en este caso los hijos se almacenan a la izquierda
+        # para que en la próxima iteración sean los primeros en ser obtenidos de la cola.
+        frontier.extendleft(child for child in node.expand(problem)
+                            if child.state not in explored and child not in frontier)
+
+    return None
